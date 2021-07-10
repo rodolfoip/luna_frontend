b6247b03a21def4fa4bc580c31e6b53177295b82
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-10 16:50:32 -0300

    feat: init project

---
+++ src/pages/PreLogin/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pre-login">
+    <v-container>
+      <v-row align="center" justify="space-between">
+        <v-col> Página de pré login </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style lang="scss" src="./style.scss"></style>
---
+++ src/pages/PreLogin/script.js
@@ -0,0 +1,4 @@
+export default {
+  name: "PreLogin",
+  components: {},
+};
