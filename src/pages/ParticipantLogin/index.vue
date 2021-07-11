8ab35b18b54a158debf5c2363aec0f594f2f4d6e
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-10 22:10:23 -0300

    feat: added participant form login

---
+++ src/pages/ParticipantLogin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login-participant">
+    <v-container>
+      <v-row justify="center">
+        <v-col sm="6" lg="3">
+          <v-card elevation="0" class="login-participant__form">
+            <h4 class="form__title">Acessar teste</h4>
+            <div class="form__subtitle">
+              É necessário ter um código de acesso disponibilizado pelo
+              avaliador
+            </div>
+            <v-form>
+              <v-text-field
+                class="form__input"
+                dense
+                v-model="accessCode"
+                label="Código de acesso"
+                required
+              ></v-text-field>
+            </v-form>
+            <v-btn color="primary" elevation="2">Entrar</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style lang="scss" src="./style.scss"></style>
