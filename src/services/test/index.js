5c10f7d8f6d88d537a64de2c6b26f6b43b652111
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-09-25 12:07:33 -0300

    feat: added usability test API methods

--- src/services/test/index.js
+++ src/services/test/index.js
@@ -17,3 +17,35 @@ export const registerTest = ({
 export const listTests = () => {
   return API.get("/usability-test");
 };
+
+export const getTestById = (id) => {
+  return API.get(`/usability-test/${id}`);
+};
+
+export const getTestByName = (name) => {
+  return API.get(`/usability-test/name/${name}`);
+};
+
+export const getTestByAccessCode = (accessCode) => {
+  return API.get(`/usability-test/accessCode/${accessCode}`);
+};
+
+export const updateTest = ({
+  _id,
+  name,
+  accessCode,
+  externalLink,
+  prototypeLink,
+}) => {
+  return API.put("/usability-test", {
+    _id,
+    name,
+    accessCode,
+    externalLink,
+    prototypeLink,
+  });
+};
+
+export const deleteTest = (id) => {
+  return API.delete("/usability-test", { id });
+};
