75e592bcb3a857862a4db14585e01f8cf6443ada
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-10-16 08:48:42 -0300

    feat: exporting delete tasks by services

--- src/services/index.js
+++ src/services/index.js
@@ -2,4 +2,4 @@ export { API } from "./config";
 
 export { login, register } from "./appraiser";
 export { registerTest, getTestById } from "./test";
-export { registerTask, updateTask } from "./task";
+export { registerTask, updateTask, deleteTask } from "./task";
